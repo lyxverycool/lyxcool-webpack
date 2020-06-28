108657aea373f8bdd15351c9f96573ea9cdd277d
cool.li <liang@vpgame.cn>
2020-06-28 11:57:46 +0800

    '2.0.0'

--- src/webpack.dev.conf.js
+++ src/webpack.dev.conf.js
@@ -15,7 +15,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, '../dist/'),
     host: 'localhost',
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     hot: true,
     overlay: true,
     historyApiFallback: true,
